ec826053cde0d2dc44b4dce4386bd682aadecd59
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-08-14 18:26:26 -0300

    Persiste sessão no banco

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,17 +2,36 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+	}
+	
+	/**
+	 * @deprecated Hibernate only
+	 */
+	public Sessao() {
+		// TODO Auto-generated constructor stub
 	}
 	
 	public Integer getId() {
