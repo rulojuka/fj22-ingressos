754f923f2c53be09156ee2da3448f47afbd5a0ce
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-08-15 22:41:17 -0300

    [WIP] Adiciona alguns dos testes do Gerenciador de Sessoes
    Co-Authored-by: Raimundo Morais <jraimundo.smorais@gmail.com>
    Co-Authored-by: Diogo Ramos <dramoslopesilveira@gmail.com>
    Co-Authored-by: Alan Nascimento <oquealan@gmail.com>
    Co-Authored-by: Leandro Ibraim

---
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadordeSessaoTest.java
@@ -0,0 +1,50 @@
+package br.com.caelum.ingresso.validacao;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadordeSessaoTest {
+
+	private Filme filme;
+	private Sala sala;
+	private LocalTime horario;
+	private Sessao sessao;
+
+	@Before
+	public void setup() {
+		filme = new Filme("Taxi3", Duration.ofMinutes(45), "Ação");
+		sala = new Sala("Sala 01");
+		horario = LocalTime.parse("23:30:00");
+		sessao = new Sessao(horario, sala, filme);
+	}
+	
+	@Test
+	public void naoDeveCaberQuandoASessaoComecaEmUmDiaETerminaNoOutro() {
+		GerenciadordeSessao gerenciadordeSessao = new GerenciadordeSessao(Arrays.asList(sessao));
+		
+		boolean cabe = gerenciadordeSessao.cabe(sessao);
+
+		assertFalse(cabe);
+	}
+
+	@Test
+	public void deveCaberQuandoAListaEstiverVazia() {
+		List<Sessao> listaVazia = new ArrayList<Sessao>();
+		new GerenciadordeSessao(listaVazia);
+	}
+}
