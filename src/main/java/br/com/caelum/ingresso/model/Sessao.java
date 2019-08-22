27a1a687244cac586a478e796a63052a24146ed1
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-08-22 09:42:36 -0300

    Exercício: Criando lógica para tela de seleção de lugares

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -38,6 +40,10 @@ public class Sessao {
 		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
+	}
+	
 	/**
 	 * @deprecated Hibernate only
 	 */
