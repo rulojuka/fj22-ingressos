e835044227580f70c6c419d093e1b1f3353facb6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-08-30 15:34:17 -0300

    Exercício: Enviando e-mail

--- src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
+++ src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
@@ -1,12 +1,24 @@
 package br.com.caelum.ingresso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.helper.TokenHelper;
+import br.com.caelum.ingresso.mail.EmailNovoUsuario;
+import br.com.caelum.ingresso.mail.Mailer;
+import br.com.caelum.ingresso.mail.Token;
+
 @Controller
 public class UsuarioController {
+
+	@Autowired
+	private Mailer mailer;
+
+	@Autowired
+	private TokenHelper tokenHelper;
 
 	@GetMapping("/usuario/request")
 	public ModelAndView formSolicitacaoDeAcesso() {
@@ -15,6 +27,9 @@ public class UsuarioController {
 
 	@PostMapping("/usuario/request")
 	public ModelAndView solicitacaoDeAcesso(String email) {
-		return new ModelAndView("usuario/adicionado");
+		ModelAndView modelAndView = new ModelAndView("usuario/adicionado");
+		Token token = tokenHelper.generateFrom(email);
+		mailer.send(new EmailNovoUsuario(token));
+		return modelAndView;
 	}
 }
